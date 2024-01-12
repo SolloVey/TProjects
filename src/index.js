b56166f717b31f5d1e5c35cf909cb653c946a87d
SolloVey <chelnokow@gmail.com>
2024-01-12 04:46:34 +0300

    Add widgets

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import './styles/reset.css';
 import './styles/main.scss';
 
+import Chart from 'chart.js/auto';
+
 console.log('STARTED');
+
+const firstCanvas = document.querySelector('.widget__main__canvas');
+
+// const firstData = [
+// 	{ name: 'Александра', volume: 55 },
+// 	{ name: 'Ангелина Сейт', volume: 20 },
+// 	{ name: 'Денис', volume: 10 },
+// 	{ name: 'Тимур', volume: 10 },
+// 	{ name: 'Владимир', volume: 5 },
+// ];
+
+// const firstData = {
+// 	labels:
+// };
+// const firstConfig = {
+// 	type: 'doughnut',
+// 	data: firstData,
+// };
+
+// let circleChart = new Chart(firstCanvas, firstConfig);
+
+// console.log(firstCanvas);
