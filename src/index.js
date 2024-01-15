018980832b71750a56f11d300101b7ed9c53e0c5
SolloVey <chelnokow@gmail.com>
2024-01-15 03:04:51 +0300

    Add side bar open menu

--- src/index.js
+++ src/index.js
@@ -23,15 +23,69 @@ const addBtn = document.querySelector('.sidebar-horizontal__action__add');
 const activeItemsSubMenu = document.querySelectorAll('.sub-menu__item');
 const subMenu = document.querySelectorAll('.sub-menu');
 const subMenuSvgFooter = document.querySelector('.sub-menu__svg');
+const sideBarMenuBTN = document.querySelector('.sidebar__item__todo');
+const sideBarMenuList = document.querySelector('.sidebar-menu');
+const sideBarMenuCloseBTN = document.querySelector(
+	'.sidebar-menu__header__content__close-btn'
+);
+
+// ПЕРВЫЙ ГРАФИК
+const dataTasks = [
+	{ name: 'Александра', volume: 55, units: '%' },
+	{ name: 'Владимир', volume: 5, units: '%' },
+	{ name: 'Тимур', volume: 10, units: '%' },
+	{ name: 'Денис', volume: 10, units: '%' },
+	{ name: 'Ангелина Сейт', volume: 20, units: '%' },
+];
+let name = dataTasks.map(item => `${item.name} (${item.volume}${item.units})`);
+let volume = dataTasks.map(item => item.volume);
+const createCircleChart = (name, volume) => {
+	const firstData = {
+		labels: name,
+		datasets: [
+			{
+				data: volume,
+				backgroundColor: [
+					'rgb(58, 158, 255)',
+					'rgb(0, 188, 212)',
+					'rgb(255, 179, 0)',
+					'rgb(16, 171, 79)',
+					'rgb(103, 58, 183)',
+				],
+				hoverOffset: 4,
+				rotation: 72,
+			},
+		],
+	};
+
+	const firstConfig = {
+		type: 'doughnut',
+		data: firstData,
+		options: {
+			plugins: {
+				legend: {
+					display: true,
+					position: 'bottom',
+					align: 'start',
+					labels: {
+						boxWidth: 18,
+						boxHeight: 18,
+						padding: 13,
+						usePointStyle: true,
+						pointStyle: 'circle',
+						useBorderRadius: true,
+						borderRadius: 0,
+					},
+				},
+			},
+		},
+	};
+	let chart = new Chart(firstCanvas, firstConfig);
+};
 
 // console.log(firstCanvas);
 
-function toggleSubMenu(event) {
-	// console.log(event.target.classList.contains);
-
-	// if (event.target.classList.contains(sub - menu)) {
-
-	// }
+function toggleSubMenu() {
 	subMenu.forEach(item => {
 		changeColorSvg();
 		item.classList.toggle('sub-menu__open');
@@ -63,6 +117,14 @@ function changeColorSvg() {
 	} else {
 		fillColorSVG.style.fill = 'rgb(255, 255, 255)';
 	}
+}
+
+function showSubMenuList() {
+	sideBarMenuList.classList.add('sidebar-menu_action');
+}
+
+function closeSubMenuList() {
+	sideBarMenuList.classList.remove('sidebar-menu_action');
 }
 
 activeItemsSubMenu.forEach(elem => {
@@ -97,74 +159,8 @@ activeItemsSubMenu.forEach(elem => {
 	});
 });
 
-// ПЕРВЫЙ ГРАФИК
-const dataTasks = [
-	{ name: 'Александра', volume: 55 },
-	{ name: 'Владимир', volume: 5 },
-	{ name: 'Тимур', volume: 10 },
-	{ name: 'Денис', volume: 10 },
-	{ name: 'Ангелина Сейт', volume: 20 },
-];
-
-let name = dataTasks.map(item => `${item.name} (${item.volume}%)`);
-let volume = dataTasks.map(item => item.volume);
-
-// console.log(name);
-
-const createCircleChart = (name, volume) => {
-	const firstData = {
-		labels: name,
-		datasets: [
-			{
-				data: volume,
-				backgroundColor: [
-					'rgb(58, 158, 255)',
-					'rgb(0, 188, 212)',
-					'rgb(255, 179, 0)',
-					'rgb(16, 171, 79)',
-					'rgb(103, 58, 183)',
-				],
-				hoverOffset: 4,
-				rotation: 72,
-			},
-		],
-	};
-
-	const firstConfig = {
-		type: 'doughnut',
-		data: firstData,
-		options: {
-			plugins: {
-				legend: {
-					display: true,
-					position: 'bottom',
-					align: 'start',
-					maxWidth: 500,
-					// maxHeight: 70,
-					// fullSize: true,
-					labels: {
-						boxWidth: 18,
-						boxHeight: 18,
-						usePointStyle: true,
-						pointStyle: 'circle',
-						useBorderRadius: true,
-						borderRadius: 0,
-						padding: 13,
-					},
-				},
-			},
-			// layout: {
-			// 	padding: 50,
-			// },
-		},
-	};
-	let chart = new Chart(firstCanvas, firstConfig);
-};
 createCircleChart(name, volume);
-
-// moreBtn.addEventListener('click', changeColorSvg);
 moreBtn.addEventListener('click', toggleSubMenu);
-// addBtn.addEventListener('click', toggleSubMenu);
 document.addEventListener('keydown', escapeDown);
-
-// document.body.addEventListener('click', closeSubMenu);
+sideBarMenuBTN.addEventListener('click', showSubMenuList);
+sideBarMenuCloseBTN.addEventListener('click', closeSubMenuList);
