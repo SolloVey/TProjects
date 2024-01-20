bb2ccbea1115c5e010f5f177c36a473a60dbc6d0
SolloVey <chelnokow@gmail.com>
2024-01-20 19:43:59 +0300

    Add toggle svg color icon

--- src/index.js
+++ src/index.js
@@ -36,8 +36,6 @@ const firstCanvas = document
 const activeItemsSubMenu = document.querySelectorAll('.sub-menu__item');
 const subMenu = document.querySelectorAll('.sub-menu');
 const subMenuSvgFooter = document.querySelector('.sub-menu__svg');
-const sideBarMenuBTN = document.querySelector('.sidebar__item__todo');
-const sideBarMenuList = document.querySelector('.sidebar-menu');
 const sideBarMenuCloseBTN = document.querySelector(
 	'.sidebar-menu__header__content__close-btn'
 );
@@ -45,17 +43,31 @@ const sideBarMenuCloseBTN = document.querySelector(
 // const widgetHeaderDesc = document.querySelector('.widget__header__desc');
 const widget = document.querySelector('.widget__active');
 
-// ***** sidebar *****
-const sideBarItems = document.querySelectorAll('.sidebar__el');
-// ********************
 // ***** calendar *****
 const inputTaskCalendar = document.querySelector('.widget__header__desc__date');
 const calendar = document.querySelector('.task-calendar');
 // ********************
+
+// ***** sidebar *****
+const sideBarMenuBTN = document.querySelector(
+	'.sidebar__item__todo'
+); /* картинка в элементе */
+const sideBarItemList =
+	document.querySelector('.sidebar__items'); /* весь список ul */
+const sideBarItems =
+	document.querySelectorAll('.sidebar__el'); /* все эл. в списке */
+const sideBarItem = document.querySelector('.sidebar__el'); /* эл. в списке */
+// ********************
+
 // ***** set-user *****
 const userBtn = document.querySelector('.sidebar__item__logo');
 const userMenu = document.querySelector('.set-user');
 // ********************
+
+// ***** side-bar *****
+const sideBarMenuList = document.querySelector('.sidebar-menu');
+// ********************
+
 // ***** support *****
 const support = document.querySelector('.user-support');
 const supportBtn =
@@ -63,12 +75,14 @@ const supportBtn =
 const closeSupportBtn = document.querySelector('.user-support__header__close');
 const supportImg = document.querySelectorAll('.user-support__item__icon__img');
 // **********************
+
 // ***** popup *****
 const popupLinks = document.querySelectorAll('.popup-open');
 const deleteWidgetBtn = document.querySelector('.delete-widget');
 // const removeWidget = document.querySelectorAll('.popup-close');
 const closePopupBtn = document.querySelector('.close-popup');
 // *****************
+
 // ***** horizontal side bar *****
 const sideBarMoreBtn = document.querySelector(
 	'.sidebar-horizontal__action__more'
@@ -79,6 +93,7 @@ const horizontalSideBarList = document.querySelector(
 );
 // *******************************
 
+// ***** HORISONTAL SIDE-BAR *****
 function openHorizontalSideBarMenu() {
 	sideBarMoreList.classList.add('sidebar-horizontal-sub__open');
 }
@@ -99,11 +114,15 @@ function selectedItemHorizontalSideBar(e) {
 	}
 	closeHorizontalSideBarMenu();
 }
+// *******************************
 
+// ******* CALENDAR ***********
 function showCalendar() {
 	calendar.classList.toggle('none');
 }
+// ****************************
 
+// ******* POP-UP**************
 if (popupLinks.length > 0) {
 	for (let i = 0; i < popupLinks.length; i++) {
 		const popupLink = popupLinks[i];
@@ -124,6 +143,7 @@ function popupClose() {
 	let curentPopup = document.querySelector('.popup__open');
 	curentPopup.classList.remove('popup__open');
 }
+// ****************************
 
 function removeWidget() {
 	widget.closest('.widget__active').classList.add('widget__remove');
@@ -208,18 +228,20 @@ function toggleSubMenu(e) {
 			}]`
 		)
 		.classList.toggle('sub-menu__open');
-	changeColorSvg(e);
+
+	if (e.target.closest('.svg-el')) {
+		toggleColorIcon(e);
+	}
 }
 
 function closeSubMenu() {
 	subMenu.forEach(item => {
 		item.classList.remove('sub-menu__open');
-		// console.log(item);
-
-		// changeColorSvg(item);
 	});
+	// whiteColorIcon();
 }
 
+// ********************** СМЕНА ЦВЕТА SVG *****************************************************
 activeItemsSubMenu.forEach(elem => {
 	elem.addEventListener('mouseenter', () => {
 		elem.classList.add('sub-menu__item_active');
@@ -252,49 +274,89 @@ activeItemsSubMenu.forEach(elem => {
 	});
 });
 
-function changeColorSvg(e) {
-	// console.log(e.target);
-	// fillColorsSVG.forEach(function (e) {
-	// if (e.parentElement.firstElementChild.style.fill == 'rgb(255, 255, 255)') {
-	// 	e.parentElement.firstElementChild.style.fill = 'rgb(31, 142, 250)';
-	// } else {
-	// 	e.parentElement.firstElementChild.style.fill = 'rgb(255, 255, 255)';
-	// }
-	// });
-	// if (e.style.fill == 'rgb(255, 255, 255)') {
-	// 	e.style.fill = 'rgb(31, 142, 250)';
-	// } else {
-	// 	e.style.fill = 'rgb(255, 255, 255)';
-	// }
+function toggleColorIcon(e) {
+	document.querySelector(
+		`.fill-color[data-fill-color=${
+			e.target.closest('.svg-el').dataset.fillColor
+		}]`
+	).style.fill = '#3A9EFF';
+
+	// sideBarItems.forEach(
+	// 	e => {
+	// 		if (!e.classList.contains('sidebar__item__active')) {
+	// 			// console.log(e.contains('.sidebar__alert'));
+	// 		}
+	// 	}
+	// 	// console.log(e.classList.contains('sidebar__item__active')) /* true */
+	// );
+
+	// 	document.querySelector(
+	// 		`.fill-color[data-fill-color=${
+	// 			e.target.closest('.sidebar__el').dataset.fillColor
+	// 		}]`
+	// 	).style.fill = 'fill: rgb(255, 255, 255)';
+
+	// console.log(e.target.closest('.svg-el'));
+	// console.log(e);
+	// console.log(
+	// 	document.querySelector(
+	// 		`.fill-color[data-fill-color=${
+	// 			e.target.closest('.sidebar__el').dataset.fillColor
+	// 		}]`
+	// 	)
+	// );
+	// console.log(e.target.closest('.sidebar__el').dataset.fillColor);
 }
+
+function whiteColorIcon(e) {
+	// document.querySelector(
+	// 	`.fill-color[data-fill-color=${
+	// 		e.target.closest('.svg-el').dataset.fillColor
+	// 	}]`
+	// ).style.fill = '#FFFFFF';
+
+	document.querySelector(
+		`.fill-color[data-fill-color=${e.closest('.svg-el').dataset.fillColor}]`
+	).style.fill = '#FFFFFF';
+
+	// console.log(e);
+}
+// *********************************************************************************************
+
+// ********************** ПРАВЫЙ SIDE-BAR ******************************************************
 
 function selectedItemSideBar(e) {
+	// ********************************************************************************
+	// написать ф-ию работающую по всем элементам списка
 	if (e.target.closest('.sidebar__item')) {
+		closeUserMenu(e);
 		e.target.closest('.sidebar__item').classList.add('sidebar__item__active');
+		e.target.closest('.sidebar__el').classList.remove('sidebar__under-line');
+		// console.log(e.target.closest('.sidebar__el'));
+		toggleColorIcon(e);
 	}
+	// ********************************************************************************
 }
 
+// ***** ADD ALERT *****
 function addAlertIconSideBar(e) {
 	e.target
 		.closest('.sidebar__item__todo')
 		.firstElementChild.classList.add('sidebar__alert_visible');
 }
+// *********************
 
 function showSubMenuList(e) {
-	closeUserMenu(e);
-
 	// ***************** ПЕРЕПИСАТЬ Ф-ИЮ КЛИКА ПО ЭЛЕМЕНТА SIDE-BAR ****************************
 	// if (e.target.closest('.sidebar__item')) {
-	// 	// console.log(e.target.closest('.sidebar__item'));
 	// 	e.target.closest('.sidebar__item').classList.add('sidebar__item__active');
+	// 	e.target.closest('.sidebar__el').classList.remove('sidebar__under-line');
 	// }
 	selectedItemSideBar(e);
 	// *******************************************************************************************
 	// console.log(e.target);
 	sideBarMenuList.classList.add('sidebar-menu_action');
-	// ******************* ADD ALERT ***************************************************************
 	addAlertIconSideBar(e);
-	// *********************************************************************************************
 	// changeColorSvg();
 
 	// e.target.firstElementChild.style.fill = '#1F8EFA';
@@ -308,6 +370,10 @@ function closeSubMenuList() {
 	sideBarItems.forEach(e => {
 		if (e.classList.contains('sidebar__item__active')) {
 			e.classList.remove('sidebar__item__active');
+			e.classList.add('sidebar__under-line');
+			// console.log(e);
+			// console.log(e.classList);
+			whiteColorIcon(e);
 		}
 	});
 }
@@ -328,10 +394,13 @@ function toggleUserMenu() {
 	userMenu.classList.toggle('set-user__open');
 }
 
+// *********************************************************************************************
+
 // function toggleColorSupportImg() {}
 
 createCircleChart(name, volume);
 sideBarMenuBTN.addEventListener('click', showSubMenuList);
+// sideBarItemList.addEventListener('click', showSubMenuList);
 sideBarMenuCloseBTN.addEventListener('click', closeSubMenuList);
 supportBtn.addEventListener('click', toggleSupport);
 closeSupportBtn.addEventListener('click', closeSupport);
